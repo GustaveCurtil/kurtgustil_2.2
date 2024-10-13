8e6964c58dd2e8b08bcc22632b87eb84054ae5b7
Gustave Curtil <gustave.curtil@tutanota.com>
2024-10-13 15:13:10 +0200

    nieuwe structuur voor websites te tonen, lookin' good already

--- portfolio.js
+++ portfolio.js
@@ -1,17 +1,40 @@
-let websiteKnoppen = document.querySelectorAll('#websites div');
 let websites = document.querySelectorAll('#website article');
+let selecties = document.querySelectorAll("#selectie div");
+let links = document.querySelector("#selectie svg:first-child");
+let rechts = document.querySelector("#selectie svg:last-child");
+let volgordenummer = 0;
 
-for (let i = 0; i < websiteKnoppen.length; i++) {
-    const knop = websiteKnoppen[i];
-    const website = websites[i]
-    knop.addEventListener('click', (e) => {
-        websiteKnoppen.forEach(knop => {
-            knop.classList.remove('actief');
-        });
-        knop.classList.add('actief');
-        websites.forEach(website => {
-            website.classList.remove('actief');
-        })
-        website.classList.add('actief');
+links.addEventListener('click', (e) => {
+    if (volgordenummer > 0) {
+        volgordenummer -= 1;
+    } else {
+        volgordenummer = websites.length - 1;
+    }
+    updateWebite(volgordenummer);
+})
+
+rechts.addEventListener('click', (e) => {
+    if (volgordenummer < (websites.length - 1)) {
+        volgordenummer += 1;
+    } else {
+        volgordenummer = 0;
+    }
+    updateWebite(volgordenummer);
+})
+
+
+function updateWebite(volgordenummer){
+    let website = websites[volgordenummer];
+    let selectie = selecties[volgordenummer];
+
+    selecties.forEach(selectie => {
+        selectie.classList.remove('actief');
     })
+    websites.forEach(website => {
+        website.classList.remove('actief');
+    })
+    website.classList.add('actief');
+    selectie.classList.add('actief');
+
 }
+
