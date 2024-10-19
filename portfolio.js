de283c1546958fb25951165793031301da72f902
Gustave Curtil <gustave.curtil@tutanota.com>
2024-10-19 22:14:58 +0200

    mobile portfolio bijna af

--- portfolio.js
+++ portfolio.js
@@ -1,40 +1,35 @@
 let pagina = document.querySelector('#websites');
-let websites = document.querySelectorAll('#website article');
+let websites = document.querySelectorAll('#websites article');
 let links = document.querySelector("#selectie svg:first-child");
 let rechts = document.querySelector("#selectie svg:last-child");
 let infoKnop = document.querySelector("#info");
-let hyperlinkKnop = document.querySelector("#hyperlinks");
-let hyperlinks = document.querySelectorAll("#hyperlinks a");
 const root = document.querySelector(':root');
-let uitleg = document.querySelectorAll('article .uitleg');
+let beschrijvingen = document.querySelectorAll('article .uitleg');
+let uitleg = false;
+
 
 let volgordenummer = 0;
 
 updateWebite(volgordenummer)
 
 infoKnop.addEventListener('click', (e) => {
-    pagina.classList.add("informatie");
-    infoKnop.classList.remove("actief");
-    hyperlinks[volgordenummer].style.display = "inline";
-    hyperlinkKnop.classList.add("actief");
-    uitleg[volgordenummer].style.display = "block";
+    if (!uitleg) {
+        pagina.classList.add("informatie");
+        infoKnop.classList.add("actief");
+        uitleg = true;
+    } else {
+        pagina.classList.remove("informatie");
+        infoKnop.classList.remove("actief");
+        uitleg = false;
+    }
 })
 
-function resetInfo(){
-    pagina.classList.remove("informatie");
-    hyperlinkKnop.classList.remove("actief");
-    infoKnop.classList.add("actief");
-    hyperlinks[volgordenummer].style.display = "none";
-    uitleg[volgordenummer].style.display = "none";
-}
 
 links.addEventListener('click', (e) => {
-    resetInfo();
     vorige();
 })
 
 rechts.addEventListener('click', (e) => {
-    resetInfo();
     volgende()
 })
 
@@ -78,9 +73,9 @@ function kleurtje(richting) {
 
 function updateWebite(volgordenummer){
     let website = websites[volgordenummer];
-    let hue = 50 + volgordenummer * (360 / websites.length);
-    let kleur = "hsl(" + hue + ", 20%, 47%)";
-    let kleurAchtergrond = "hsl(" + hue + ", 30%, 90%)";
+    let hue = 69 + volgordenummer * (360 / websites.length);
+    let kleur = "hsl(" + hue + ", 20%, 69%)";
+    let kleurAchtergrond = "hsl(" + hue + ", 69%, 90%)";
 
     root.style.setProperty('--kleur-browser', kleur);
     root.style.setProperty('--kleur-browser-licht', kleurAchtergrond);
