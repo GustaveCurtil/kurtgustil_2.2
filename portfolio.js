f14317c9ab889285fab53332685815180d764d2b
Gustave Curtil <gustave.curtil@tutanota.com>
2024-10-19 11:41:57 +0200

    beetje vanalles herzien eigenlijk feitelijk

--- portfolio.js
+++ portfolio.js
@@ -1,38 +1,58 @@
+let pagina = document.querySelector('#websites');
 let websites = document.querySelectorAll('#website article');
-let selecties = document.querySelectorAll("#selectie div");
 let links = document.querySelector("#selectie svg:first-child");
 let rechts = document.querySelector("#selectie svg:last-child");
-let pagina = document.querySelector("main");
+let infoKnop = document.querySelector("#info");
+let hyperlinkKnop = document.querySelector("#hyperlinks");
+let hyperlinks = document.querySelectorAll("#hyperlinks a");
 
 let volgordenummer = 0;
-let touchstartX = 0;
-let touchendX = 0;
 
+infoKnop.addEventListener('click', (e) => {
+    pagina.classList.add("informatie");
+    infoKnop.classList.remove("actief");
+    hyperlinks[volgordenummer].style.display = "inline";
+    hyperlinkKnop.classList.add("actief");
+})
+
+function resetInfo(){
+    pagina.classList.remove("informatie");
+    hyperlinkKnop.classList.remove("actief");
+    infoKnop.classList.add("actief");
+    hyperlinks[volgordenummer].style.display = "none";
+    hyperlinkKnop
+}
 
 links.addEventListener('click', (e) => {
+    resetInfo();
     vorige();
 })
 
 rechts.addEventListener('click', (e) => {
+    resetInfo();
     volgende()
 })
 
 function vorige() {
+
     if (volgordenummer > 0) {
         volgordenummer -= 1;
     } else {
         volgordenummer = websites.length - 1;
     }
+
     kleurtje(links);
     updateWebite(volgordenummer);
 }
 
 function volgende() {
+
     if (volgordenummer < (websites.length - 1)) {
         volgordenummer += 1;
     } else {
         volgordenummer = 0;
     }
+
     kleurtje(rechts);
     updateWebite(volgordenummer);
 
@@ -41,8 +61,10 @@ function volgende() {
 
 function kleurtje(richting) {
     let hue = Math.floor(Math.random() * 360) 
+
     richting.style.transition = "none";
     richting.style.backgroundColor = "hsl(" + hue + ", 21%, 52%)";
+
     setTimeout(function() {
         richting.style.transition = "background-color 1s ease";
         richting.style.backgroundColor = "";  // Reset to original color
@@ -51,39 +73,12 @@ function kleurtje(richting) {
 
 function updateWebite(volgordenummer){
     let website = websites[volgordenummer];
-    let selectie = selecties[volgordenummer];
 
-    // selecties.forEach(selectie => {
-    //     selectie.classList.remove('actief');
-    // })
     websites.forEach(website => {
         website.classList.remove('actief');
     })
     website.classList.add('actief');
-    // selectie.classList.add('actief');
-
 }
 
-pagina.addEventListener('touchstart', (e) => {
-    touchstartX = e.changedTouches[0].clientX;
-}, false);
-
-pagina.addEventListener('touchend', (e) => {
-    touchendX = e.changedTouches[0].clientX;
-    handleGesure();
-}, false);
-
-function handleGesure() {
-    if ((-30 > touchendX - touchstartX) || 30 < (touchendX - touchstartX)) {
-        if (touchendX < touchstartX) {
-            pagina.scrollTop = 0;
-            volgende();
-        }
-        if (touchendX > touchstartX) {
-            pagina.scrollTop = 0;
-            vorige();
-        }
-    }
-}
 
 
